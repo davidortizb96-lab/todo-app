c36db8ddf35a0197702d86ee8c34af54fd7064e6
David Ortiz <davidortizb.96@gmail.com>
2026-02-14 23:02:51 -0500

    Estructura base de la app con navegación y estados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import React, {useState} from 'react';
+import Inicio from "./components/Inicio";
+import CrearTarea from "./components/CrearTarea";
+import ListaTareas from "./components/ListaTareas"
 
 function App() {
+  // Estado que controla qué vista se muestra 
+  const [vista, setVista] = useState("inicio");
+  // Estado que almacena las tareas
+  const [tareas, setTareas] = useState([]);
+
+  // Función para agregar una nueva tarea
+  const agregarTarea = (nuevaTarea) => {
+    setTareas([...tareas,nuevaTarea]);
+    setVista("lista");
+  };
+
+  // Renderizado condicional
+  if (vista === "crear") {
+    return <CrearTarea onAgregar={agregarTarea} onVolver={() => setVista("inicio")} />;
+  }
+   if (vista === "lista") {
+    return <ListaTareas tareas={tareas} onVolver={() => setVista("inicio")} />;
+  }
+
   return (
-    <div>
-      <h1>To-Do App</h1>
-    </div>
+    < Inicio onCrear={() => setVista("crear")} onVerLista={() => setVista("lista")}/>
   );
 }
 
